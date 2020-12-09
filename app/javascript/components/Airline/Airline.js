d7004bb29fdae49246031c2cfb0e22666b239426
Dave Dziak <davidjdziak@gmail.com>
2020-12-09 11:12:45 -0500

    fixingnode modules to let react compile

---
+++ app/javascript/components/Airline/Airline.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Airline = () => {
+  return <div>This is the airline#show view for our app</div>;
+};
+
+export default Airline;
---
+++ app/javascript/components/Airlines/Airlines.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Airlines = () => {
+  return <div>This is the airlines#index view for our app</div>;
+};
+
+export default Airlines;
