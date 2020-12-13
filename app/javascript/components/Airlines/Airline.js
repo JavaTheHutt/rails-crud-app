fd5d47720d9c9a0d4ee272e4c26edaf24bf4a6e5
Dave Dziak <davidjdziak@gmail.com>
2020-12-13 16:26:29 -0500

    add styling, remove turbolinks

--- app/javascript/components/Airlines/Airline.js
+++ app/javascript/components/Airlines/Airline.js
@@ -1,18 +1,53 @@
 import React from "react";
 import { BrowserRoute as Router, Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Card = styled.div`
+  border: 1px solid #efefef;
+  background #fff;
+  text-align: center;
+`;
+const AirlineLogo = styled.div`
+  width: 50px img {
+    height: 50px;
+    width: 50px;
+    border-radius: 100%;
+    border: 1px solid #efefef;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 10px;
+  }
+`;
+const AirlineName = styled.div`
+  padding: 20px 0 10px 0;
+`;
+const LinkWrapper = styled.div`
+  margin: 30px 0 20px 0;
+  height: 50px;
+
+  a {
+    color: #fff;
+    background: #000;
+    border-radius: 4px;
+    padding: 10px 50px;
+    border: 1px solid #000;
+    width: 100%;
+    text-decoration: none;
+  }
+`;
 
 const Airline = (props) => {
   return (
-    <div className="card">
-      <div className="airline-logo">
+    <Card>
+      <AirlineLogo>
         <img src={props.attributes.mage_url} alt={props.attributes.name} />
-      </div>
-      <div className="airline-name">{props.attributes.name}</div>
+      </AirlineLogo>
+      <AirlineName>{props.attributes.name}</AirlineName>
       <div className="airline-score">{props.attributes.avg_score}</div>
-      <div className="airline-link">
+      <LinkWrapper>
         <Link to={`/airlines/${props.attributes.slug}`}>View Airline</Link>
-      </div>
-    </div>
+      </LinkWrapper>
+    </Card>
   );
 };
 
--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,6 +1,32 @@
 import React, { useState, useEffect, Fragment } from "react";
 import axios from "axios";
 import Airline from "./Airline";
+import styled from "styled-components";
+
+const Home = styled.div`
+  text-align: center;
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+const Header = styled.div`
+  padding: 100px 100px 10px 100px;
+
+  h1 {
+    font-size: 42px;
+  }
+`;
+const Subheader = styled.div`
+  font-weight: 300;
+  font-sixe: 26px;
+`;
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  width: 100%;
+  passing: 20px;
+`;
 
 const Airlines = () => {
   const [airlines, setAirlines] = useState([]);
@@ -21,13 +47,13 @@ const Airlines = () => {
   });
 
   return (
-    <div className="home">
-      <div className="header">
+    <Home>
+      <Header>
         <h1>OpenFlights</h1>
-        <div className="subheader">Honest, unbiased airline reviews</div>
-      </div>
-      <div className="grid">{grid}</div>
-    </div>
+        <Subheader>Honest, unbiased airline reviews</Subheader>
+      </Header>
+      <Grid>{grid}</Grid>
+    </Home>
   );
 };
 
